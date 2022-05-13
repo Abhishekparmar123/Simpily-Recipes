7ffee5c59c52690e6e47413b04e29ccae3534f07
abhishek <abhishekparmarjnv@gmail.com>
2022-05-13 21:55:37 +0530

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 
 import 'normalize.css';
 import '../assets/css/main.css';
